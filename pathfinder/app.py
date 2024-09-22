a11f323358a7502aa908883c4f1e8ac7ca293d7c
Wilberth Gonzalez <willgxnzalez@icloud.com>
2024-09-22 11:55:32 -0700

    Render map loading text to screen

--- pathfinder/app.py
+++ pathfinder/app.py
@@ -15,17 +15,28 @@ class App(pg.window.Window):
         self._camera = Camera(WINDOW_WIDTH, WINDOW_HEIGHT)
         
         self._map_render_batch = pg.graphics.Batch()
+        
+        self._circle = pg.shapes.Circle(100, 200, radius=40, color=(0,0,255), batch=self._map_render_batch)
+        
+        self._map_loading_text = pg.text.Label('Loading map data. Please wait...', 
+                                               font_name='Times New Roman',
+                                                font_size=36,
+                                                x=self.width//2, y=self.height//2,
+                                                anchor_x='center', anchor_y='center', batch=self._map_render_batch)
+        
     
+
     def on_draw(self) -> None:
         self.clear()
+        if not self._map_handler.is_loading_map():
+            self._map_render_batch.draw()
         
     def on_resize(self, width, height) -> None:
         super().on_resize(width, height)
         self._camera.resize(width, height)
         
     def update(self, dt: float) -> None:
-        if not self._map_handler.is_loading_map():
-            print("map finished loading")
+        pass
     
     def start(self) -> None:
         threading.Thread(target=self._map_handler.download_map, daemon=True).start()
